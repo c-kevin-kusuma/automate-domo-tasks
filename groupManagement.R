fcd62b881a64868bc46f60ce7b61988f26758c34
Christian Kevin Kusuma <73664921+c-kevin-kusuma@users.noreply.github.com>
2023-03-23 14:58:33 -0600

    Update groupManagement.R

--- groupManagement.R
+++ groupManagement.R
@@ -5,14 +5,15 @@
 # Add users to certain groups
 # ---------------------------------------------------------------------------------------
 library(tidyverse)
-library(rdomo)
-domo <- Domo(client_id='client_id', secret='secret')
+library(pRoDomo)
+amlrs_domo <- Domo(client_id='e4f0cc6d-3d15-4e13-a270-1bb0a5973e40',
+                   secret='68971f2478e0f2e1772917351f10d144e55d24db4ff36fbe366c956cc8e5cdf4')
 # ---------------------------------------------------------------------------------------
 
 # Automated List
 automated_list <- 
-  domo$ds_query('datasetID', 
-                      'select `Group ID`, `DOMO employeeId`*1 `DOMO employeeId` from table')
+  amlrs_domo$ds_query('648b8b0b-f1a1-4311-bd00-c1dab51b1a88', 
+                'select `Group ID`, `DOMO employeeId`*1 `DOMO employeeId` from table')
 
 group_list <- automated_list %>% 
   group_by(`Group ID`) %>% 
@@ -30,19 +31,19 @@ for (i in 1:nrow(group_list)) {
   group_id <- group_list[i, ]$`Group ID`
   
   # Current Users
-  current_list <- tibble(`DOMO employeeId` = domo$groups_list_users(group_id = group_id))
+  current_list <- tibble(`DOMO employeeId` = amlrs_domo$groups_list_users(group_id = group_id))
   correct_list <- group_user %>% filter(`Group ID` == group_id) %>% select(`DOMO employeeId`)
   
   # Delete Users
   delete_list <- current_list %>% anti_join(correct_list)
-  if(nrow(delete_list) > 0) {domo$groups_remove_users(group_id = group_id, users = delete_list$`DOMO employeeId`)}
+  if(nrow(delete_list) > 0) {amlrs_domo$groups_remove_users(group_id = group_id, users = delete_list$`DOMO employeeId`)}
   
   # Add Users
   add_list <- correct_list %>% anti_join(current_list)
-  if(nrow(add_list) > 0) {domo$groups_add_users(group_id = group_id, users = add_list$`DOMO employeeId`)}
+  if(nrow(add_list) > 0) {amlrs_domo$groups_add_users(group_id = group_id, users = add_list$`DOMO employeeId`)}
 }
 
 # ---------------------------------------------------------------------------------------
 
 # Record Log
-domo$ds_update("datasetID", tibble(Operation = 'Group Management', Time = Sys.time(), Status = 'Successful'))
+amlrs_domo$ds_update("a90f97f0-b0db-40a3-8909-95fac3f44e37", tibble(Operation = 'Group Management', Time = Sys.time(), Status = 'Successful'))
